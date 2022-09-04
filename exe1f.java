3610e5888da7a8f0e37db57812a2a8ad6123ec57
bibs241 <112669290+bibs241@users.noreply.github.com>
2022-09-03 21:26:47 -0300

    Create exe1f.java

---
+++ exe1f.java
@@ -0,0 +1,24 @@
+package primeiroprograma;
+
+public class PrimeiroPrograma{
+    public static void main(String[] args) {
+        int z,y,x;
+        x = 1;
+        y = 2;
+        z = y - x;
+        System.out.println(z);
+        x = 5;
+        y = x + z;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        
+        
+        
+         
+               
+            
+                
+        
+    }
+    }
