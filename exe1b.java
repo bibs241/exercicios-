77e2ea896a5a83dbe27e8dc5e929d911db6b775c
bibs241 <112669290+bibs241@users.noreply.github.com>
2022-09-03 21:16:09 -0300

    Create exe1b.java

---
+++ exe1b.java
@@ -0,0 +1,26 @@
+package primeiroprograma;
+
+public class PrimeiroPrograma{
+    public static void main(String[] args) {
+        int A,B,C;
+        A = 30;
+        B = 20;
+        C = A + B;    
+        System.out.println(C);
+        B = 10;
+        System.out.println(B);
+        System.out.println(C);
+        C += A ;
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        
+        
+        
+         
+               
+            
+                
+        
+    }
+    }
