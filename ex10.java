80e12b23fbc0aa5ed3b02ba901c1217208f2016a
bibs241 <112669290+bibs241@users.noreply.github.com>
2022-09-08 21:19:06 -0300

    Create ex10.java

---
+++ ex10.java
@@ -0,0 +1,33 @@
+public class PrimeiroPrograma{
+    public static void main(String[] args) {
+        int x,y,z;
+        x = 3;
+        y = 2;
+        
+        z = (x*y) + 5;
+        if (z <= 0 ){
+           System.out.println("resposta a");
+        }
+        else if(z <= 100 ){
+              System.out.println("resposta b");
+          }
+        else{ 
+            System.out.println("resposta c");
+        }
+          
+           
+        
+           
+         
+         
+          
+             
+    
+        
+         
+        
+        
+        
+    
+}
+  }
