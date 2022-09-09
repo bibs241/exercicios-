ad9ef5b8acab42c6c89dc47e53e248032e30cadc
bibs241 <112669290+bibs241@users.noreply.github.com>
2022-09-08 21:30:58 -0300

    Create ex7.java

---
+++ ex7.java
@@ -0,0 +1,16 @@
+public class Exercicio7 {
+ 
+ 
+    public static void main(String[] args) {
+ 
+        int numero = 8;
+        int tabuada;
+        for (int i = 1; i < 11; i++) {
+            tabuada = numero *i ;
+           System.out.println("A tabuada do 8 * " + i + " = " + tabuada);
+ 
+        }
+ 
+    }
+ 
+}
